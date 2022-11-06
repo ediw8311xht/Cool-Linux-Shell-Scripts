5d25a28f1d9b48af059537f8f8f2c442ac3bb93f
ediw8311xht <maxballard333@gmail.com>
2022-11-06 01:57:32 -0400

    stuff

--- i3wm/i3configcheck.sh
+++ i3wm/i3configcheck.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
+
+ASK() {
+	timeout "${1}" i3-input -f 'xft:Hermit 15' -l 1 -P 'SUCCESS RELOAD RESTART i3wm? (y/n)'\
+		| grep -Pi '(?<=^command = ).+$'
+	return "$?"
+}
+
+TIMEOUT="3"
+
+while [[ "${1}" =~ ^- ]] ; do
+	if   [[ "${1}" =~ ^--NOASK$  ]] ; then o_NOASK="Y"
+	elif [[ "${1}" =~ ^--[0-9]+$ ]] ; then TIMEOUT="${1: 2}"; fi
+	shift 1
+done
+
+
 OUT="$(i3 -C "${1:-"/home/maceurt/.i3/config"}")"
-if [[ "$?" -ne 0 ]] || [[ -n "${OUT}" ]] ; then
-	timeout 5 i3-input -f 'xft:Hermit 15' -l 1 -P 'Error in your config file'
-else
-	A="$(timeout 5 i3-input -f 'xft:Hermit 15' -l 1 -P 'Success: Reload & Restart i3wm? (y/n)' |\
-		grep -Pi '(?<=^command = ).+$')"
-	if [[ "${A}" =~ (y|Y) ]] ; then
-		i3-msg "reload; restart"
-		timeout 5 i3-input -l 1 -f 'xft:Hermit 15' -P "Reloaded & Restarted"
-	else
-		timeout 5 i3-input -l 1 -f 'xft:Hermit 15' -P "Not Reloading"
-	fi
+
+if 	 [[ "$?" -ne 0 ]] || [[ -n "${OUT}" ]] ; then
+	i3-input -f 'xft:Hermit 15' -l 1 -P '!![Error in your config file]!!'
+	exit 0
+elif [[ "${o_NOASK:-"$(ASK ${TIMEOUT})"}" =~ ^[^yY]*$ ]] ; then
+	timeout "${TIMEOUT}"\
+		i3-input -l 1 -f 'xft:Hermit 15' -P 'NOT Reloading/Restarting'; exit 0
+fi
+
+i3-msg 	"reload; restart"
+
+if [[ "${TIMEOUT}" -gt "0" ]] ; then
+	timeout "${TIMEOUT}"\
+		i3-input -l 1 -f 'xft:Hermit 15' -P 'Reloaded & Restarted'
 fi
--- i3wm/i3wm_close_scratchpad.sh
+++ i3wm/i3wm_close_scratchpad.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ASSUMES ALL YOUR i3wm SCRATCHPADS HAVE NAME i3wm_scratchpad_* on them
-
 GOT=$(ps ax | grep -Po '([0-9])+(?= [\?|p].+([i]3wm_scratchpad_))')
 
 kill $GOT
