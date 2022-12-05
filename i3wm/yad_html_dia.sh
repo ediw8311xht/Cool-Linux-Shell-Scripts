24a228d54b7489871245f26a59792394b4454a34
ediw8311xht <maxballard333@gmail.com>
2022-12-05 02:21:59 -0500

    Z

--- i3wm/yad_html_dia.sh
+++ i3wm/yad_html_dia.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
 
-function yad_hd() {
-    yad --title "${1:-"No Title Provided"}" --posx="1" --posy="1" --height="1" --no-focus --no-buttons --text "${2:-"${1}"}" 
+function handle_args() {
+
+    case "${1,,}" in 
+           -title) TITLE="${2}" ;;
+           -theme) THEME="${2}" ;;
+           -*);;*) TEXT="${@}"  ;return "$?"
+    esac
+
+    shift 2; handle_args "${@}"
 }
 
+function yad_hd() {
+    TITLE="No Title Provided"
+    TEXT="No Text Provided"
+
+    handle_args "${@}"
+    GTK_THEME="${THEME}"\
+        yad --title "${TITLE}" --posx="${YAD_POS_X:-"1"}" --posy="${YAD_POS_Y:-"1"}" --height="1" --no-focus --no-buttons --text "${TEXT}" 
+}
 
 yad_hd "${@}"
 
