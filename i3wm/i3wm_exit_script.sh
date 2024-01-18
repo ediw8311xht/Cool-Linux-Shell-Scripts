52e18b0e62027184f1dcb998120665c7f9379270
ediw8311xht <maxballard333@gmail.com>
2024-01-17 22:08:44 -0500

    aksdfj

--- i3wm/i3wm_exit_script.sh
+++ i3wm/i3wm_exit_script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+save_bash_history() {
+    local history_file="${HISTFILE:-"${HOME}/.bash_history"}"
+    local save_file="bash_history_$(date +'%Y%m%d_%H%M%S').txt"
+    if [[ -d "${BASH_HISTORY_ARCHIVE}" ]] ; then
+        cp "${history_file}" "${BASH_HISTORY_ARCHIVE}/${save_file}"
+    else
+        cp "${history_file}" "${HOME}/${save_file}"
+    fi
+}
+
 shut_g() {
     local count=0
-    max_iterations='10'
-    while [[ count -lt "${max_iterations}" ]] && ps ax | grep -qPi 'd[e]luged' ; do
+    local max_iterations=10
+    save_bash_history
+    while [[ "${count}" -lt "${max_iterations}" ]] && ps ax | grep -qPi 'd[e]luged' ; do
         killall 'deluged'
         sleep 1
     done
-    cp ~/.bash_history2 "${BASH_HISTORY_SAVE:-"$HOME/Documents/"}/bash_history_$(date)"
     i3-msg 'exit'
 }
 
-
-shut_g "${@}"
+save_bash_history
+#shut_g "${@}"
--- i3wm/polybar_select_dmenu.sh
+++ i3wm/polybar_select_dmenu.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-tmpfile=$(mktemp /tmp/mdmenu.XXXXXX)
-trap 'rm "$tmpfile"' 0 1 15
-cd "$HOME/.config/polybar/configs/" || return "$?"
-find ./* -type f >> "${tmpfile}"
-choice="$(dmenu -b -nb "#000000" -i -l 10 -fn "Office Code Pro:pixelsize=20:antialias=true:autohint=true" -p "OPEN:" < "${tmpfile}")" || exit 1
+####################
+( #-START-SUBSHELL-#
+####################
 
-echo "${choice}"
+function main() {
+    tmpfile="$(mktemp /tmp/mdmenu.XXXXXX)"
+    trap "trash-put '${tmpfile}'" EXIT
+    cd "$HOME/.config/polybar/configs/" || return "$?"
+    find ./* -type f >> "${tmpfile}"
+    choice="$(dmenu -b -nb "#000000" -i -l 10 -fn "Office Code Pro:pixelsize=20:antialias=true:autohint=true" -p "OPEN:" < "${tmpfile}")" || exit 1
 
-.  "$HOME/bin/polybar_manipulate.sh" "CONFIG" "${choice}"
+    echo "${choice}"
+
+    "${HOME}/bin/polybar_manipulate.sh" "CONFIG" "${choice}"
+}
+
+main
+
+####################
+) #---END-SUBSHELL-#
+####################
