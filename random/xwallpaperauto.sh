a5e558978d1805a868021b7fcd53b1185472531a
ediw8311xht <maxballard333@gmail.com>
2024-05-27 02:44:21 -0400

    KFJAK

--- random/xwallpaperauto.sh
+++ random/xwallpaperauto.sh
@@ -4,15 +4,32 @@
 ( #-START-SUBSHELL-#
 ####################
 
+IFS=$'\n'
 L_DIRS=()
 L_PICS=()
 SILENT='0'
 DATA_FILE="$HOME/bin/Data/xwallautoDATA.txt"
+
+notify_wallpaper_change() {
+    dunstctl close-all
+    notify_args=(
+        -h string:bgcolor:"#000000"
+        -h string:fgcolor:"#00FF00"
+        -h string:frcolor:"#00FF00"
+        -t "10000"
+    )
+    string=(
+        "xwallpaper [${PARGS[*]:---focus}]"
+        "${DPOS} - ${L_DIRS[DPOS]##*/}"
+        "${PICPOS} - ${L_PICS[${PICPOS}]##*/}"
+    )
+    notify-send "${notify_args[@]}" "${string[*]}"
+}
+
 init_data_file() {
     touch "${DATA_FILE}"
-    if [[ "$(wc -l "${DATA_FILE}" | grep -o '^[0-9]*')" -lt 4 ]] ; then
-        # IF DATA FILE IS EMPTY THEN ADD LINES
-        echo $'\n\n\n\n\n\n\n' > "${DATA_FILE}"
+    if [[ "$(wc -l <"${DATA_FILE}")" -lt 4 ]] ; then
+        echo $'\n\n\n\n\n\n\n' > "${DATA_FILE}" # IF DATA FILE IS EMPTY THEN ADD LINES
     fi
 }
 
@@ -37,35 +54,22 @@ update_data_file() {
 pic_find() {
     find "${1}" \( -iname '*.png' -o -iname '*.jpg' \) -printf '%p\n' | sort | uniq
 }
+
 dirs_with_pics() {
     find "${MAIN_DIR}" -mindepth 1 -maxdepth 2 \( -iname '*.png' -o -iname '*.jpg' \) -printf '%h\n' | sort | uniq
 }
+
 handle_args() {
     case "${1,,}" in
            left) PICPOS=0 ; ((DPOS--))
     ;;    right) PICPOS=0 ; ((DPOS++))
-    ;;       up) ((PICPOS--))
-    ;;     down) ((PICPOS++))
+    ;;       up) ((PICPOS++))
+    ;;     down) ((PICPOS--))
     ;; --silent) SILENT=1
-    ;;  --pargs) PARGS=( "${2}" )
+    ;;  --pargs) PARGS=( "${2}" ); shift 1
     ;; esac
-    shift && [[ "$#" -ge 1 ]] && handle_args "${@}"
-}
-notify_wallpaper_change() {
-    IFS=$'\n'
-    dunstctl close-all
-    notify_args=(
-        -h string:bgcolor:"#000000"
-        -h string:fgcolor:"#00FF00"
-        -h string:frcolor:"#00FF00"
-        -t "10000"
-    )
-    string=(
-        "xwallpaper [${PARGS[*]:---focus}]"
-        "${DPOS} - ${L_DIRS[DPOS]}"
-        "${PICPOS} - ${L_PICS[${PICPOS}]##*/}"
-    )
-    notify-send "${notify_args[@]}" "${string[*]}"
+    shift 1
+    [[ "$#" -ge 1 ]] && handle_args "${@}"
 }
 
 main() {
@@ -75,12 +79,12 @@ main() {
 
     mapfile -t "L_DIRS" < <(dirs_with_pics)
     [[ "$(( DPOS %= "${#L_DIRS[@]}" ))" -ge 0 ]] || (( DPOS += "${#L_DIRS[@]}" ))
+
     mapfile -t "L_PICS" < <(pic_find "${L_DIRS[ "${DPOS}" ]}")
-    echo "${L_PICS[@]}"
     [[ "$(( PICPOS %= "${#L_PICS[@]}" ))" -ge 0 ]] || (( PICPOS += "${#L_PICS[@]}" ))
 
     #-----CHANGE-WALLPAPER---------------------------------------------------------#
-    xwallpaper "${PARGS[*]:---focus}" "${L_PICS[ "${PICPOS}" ]}"
+    xwallpaper "${PARGS[@]:---focus}" "${L_PICS[ "${PICPOS}" ]}"
 
     #-----UPDATE-DATA-FILE---------------------------------------------------------#
     update_data_file
