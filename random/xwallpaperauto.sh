116577e2b726efe21a39d6902ab0ea21d82d2263
ediw8311xht <maxballard333@gmail.com>
2024-05-27 02:31:11 -0400

    xwallpaperauto much better

--- random/xwallpaperauto.sh
+++ random/xwallpaperauto.sh
@@ -4,84 +4,93 @@
 ( #-START-SUBSHELL-#
 ####################
 
-#-----INIT---------------------------------------------------------------------#
-IFS=$'\n'
+L_DIRS=()
+L_PICS=()
+SILENT='0'
 DATA_FILE="$HOME/bin/Data/xwallautoDATA.txt"
-touch "${DATA_FILE}"
-if [[ "$(wc -l "${DATA_FILE}" | grep -o '^[0-9]*')" -lt 4 ]] ; then
-    # IF DATA FILE IS EMPTY THEN ADD LINES
-    echo $'\n\n\n\n\n\n\n' > "${DATA_FILE}"
-fi
+init_data_file() {
+    touch "${DATA_FILE}"
+    if [[ "$(wc -l "${DATA_FILE}" | grep -o '^[0-9]*')" -lt 4 ]] ; then
+        # IF DATA FILE IS EMPTY THEN ADD LINES
+        echo $'\n\n\n\n\n\n\n' > "${DATA_FILE}"
+    fi
+}
 
 #-------------RESERVE-FIRST-LINE--#
-main_dir="$(sed -n 2p "${DATA_FILE}")"
-dpos="$(sed -n 3p "${DATA_FILE}")"
-picpos="$(sed -n 4p "${DATA_FILE}")"
-pargs="$(sed -n 5p "${DATA_FILE}")"
-
-#-----CHECK--------------------------------------------------------------------#
-[[            -d "${main_dir}" ]] || main_dir="$HOME/Pictures/Wallpapers"
-[[   "${dpos}" =~ ^[-]?[0-9]+$ ]] || dpos=0
-[[ "${picpos}" =~ ^[-]?[0-9]+$ ]] || picpos=0
-
-#-----FUNCTIONS----------------------------------------------------------------#
-picfind='find "${1}" -mindepth 1 -maxdepth 1 -type f -iregex ".*[.]\(png\|jpg\|jpeg\)"'
-function dirs_with_pics() {
-    find "${1}" -mindepth 1 -maxdepth 1 -type d \
-        -execdir bash -c -- "${picfind}"' -exec echo "${1#./*}" \; -quit' - {} \;
+read_from_data_file() {
+    MAIN_DIR="$(    sed -n 2p "${DATA_FILE}"   )"
+    DPOS="$(        sed -n 3p "${DATA_FILE}"   )"
+    PICPOS="$(      sed -n 4p "${DATA_FILE}"   )"
+    PARGS="$(       sed -n 5p "${DATA_FILE}"   )"
+    [[        -d "${MAIN_DIR}" ]] || MAIN_DIR="$HOME/Pictures/Wallpapers"
+    [[   "${DPOS}" =~ ^[0-9]+$ ]] || DPOS=0
+    [[ "${PICPOS}" =~ ^[0-9]+$ ]] || PICPOS=0
 }
-function handle_args() {
+
+update_data_file() {
+    sed -i '2s#.*#'"${MAIN_DIR}"'#'  "${DATA_FILE}"
+    sed -i '3s#.*#'"${DPOS}"'#'      "${DATA_FILE}"
+    sed -i '4s#.*#'"${PICPOS}"'#'    "${DATA_FILE}"
+    sed -i '5s#.*#'"${PARGS[*]}"'#'   "${DATA_FILE}"
+}
+
+pic_find() {
+    find "${1}" \( -iname '*.png' -o -iname '*.jpg' \) -printf '%p\n' | sort | uniq
+}
+dirs_with_pics() {
+    find "${MAIN_DIR}" -mindepth 1 -maxdepth 2 \( -iname '*.png' -o -iname '*.jpg' \) -printf '%h\n' | sort | uniq
+}
+handle_args() {
     case "${1,,}" in
-        left) picpos=0 ; ((dpos--))
-    ;; right) picpos=0 ; ((dpos++))
-    ;;    up) ((picpos--))
-    ;;  down) ((picpos++))
+           left) PICPOS=0 ; ((DPOS--))
+    ;;    right) PICPOS=0 ; ((DPOS++))
+    ;;       up) ((PICPOS--))
+    ;;     down) ((PICPOS++))
     ;; --silent) SILENT=1
-    ;; --output) OUTPUTS+=("${2}") ; shift
-    ;;  --pargs) if [[ "${got_t}" -ne '1' ]] ; then got_t='1' ;  pargs=( "${2}" )
-                 else                                           pargs+=( "${2}" ) ; fi
+    ;;  --pargs) PARGS=( "${2}" )
     ;; esac
     shift && [[ "$#" -ge 1 ]] && handle_args "${@}"
 }
-
-#------------------------------------------------------------------------------#
-#-----MAIN---------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
-cd "${main_dir}" || exit 1
-handle_args "${@}"
-
-ldirs=(); mapfile -t "ldirs" < <(dirs_with_pics '.' '-quit' | sort)
-[[ "$(( dpos %= "${#ldirs[@]}" ))" -ge 0 ]] || (( dpos += "${#ldirs[@]}" ))
-lpics=(); mapfile -t "lpics" < <(bash -c -- "${picfind}" - "${ldirs[ "${dpos}" ]}" | sort)
-[[ "$(( picpos %= "${#lpics[@]}" ))" -ge 0 ]] || (( picpos += "${#lpics[@]}" ))
-
-#-----CHANGE-WALLPAPER---------------------------------------------------------#
-if [[ -z "${OUTPUTS[*]}" ]] ; then
-    for i in $(xrandr --listmonitors | grep -Pio "^[ \t]*[0-9]+[:].*[ ]\K[^ ]+$") ; do
-        xwallpaper --output "${i}" "${pargs[*]:---focus}" "${lpics[ "${picpos}" ]}"
-    done
-else
-    xwallpaper "${OUTPUTS[*]}" $(printf '%s' "${pargs[*]:---focus}") "${lpics[ "${picpos}" ]}"
-fi
-
-#-----UPDATE-DATA-FILE---------------------------------------------------------#
-sed -i '2s#.*#'"${main_dir}"'#'  "${DATA_FILE}"
-sed -i '3s#.*#'"${dpos}"'#'      "${DATA_FILE}"
-sed -i '4s#.*#'"${picpos}"'#'    "${DATA_FILE}"
-sed -i '5s#.*#'"${pargs[*]}"'#'     "${DATA_FILE}"
-
-#-----NOTIFICATION-------------------------------------------------------------#
-if [[ -z "${SILENT}" ]] ; then
+notify_wallpaper_change() {
+    IFS=$'\n'
     dunstctl close-all
-    string="$(printf $'%s \n'                     \
-              "xwallpaper [${pargs[*]:---focus}]" \
-              "[${dpos}] - ${ldirs[dpos]}"        \
-              "[${picpos}] - ${lpics[${picpos}]##*/}" )"
-    notify-send -h string:bgcolor:"#000000" -h string:fgcolor:"#00FF00"     \
-                -h string:frcolor:"#00FF00" -t "10000" "${string}"
-    echo "HI"
-fi
+    notify_args=(
+        -h string:bgcolor:"#000000"
+        -h string:fgcolor:"#00FF00"
+        -h string:frcolor:"#00FF00"
+        -t "10000"
+    )
+    string=(
+        "xwallpaper [${PARGS[*]:---focus}]"
+        "${DPOS} - ${L_DIRS[DPOS]}"
+        "${PICPOS} - ${L_PICS[${PICPOS}]##*/}"
+    )
+    notify-send "${notify_args[@]}" "${string[*]}"
+}
 
+main() {
+    init_data_file
+    read_from_data_file
+    handle_args "${@}"
+
+    mapfile -t "L_DIRS" < <(dirs_with_pics)
+    [[ "$(( DPOS %= "${#L_DIRS[@]}" ))" -ge 0 ]] || (( DPOS += "${#L_DIRS[@]}" ))
+    mapfile -t "L_PICS" < <(pic_find "${L_DIRS[ "${DPOS}" ]}")
+    echo "${L_PICS[@]}"
+    [[ "$(( PICPOS %= "${#L_PICS[@]}" ))" -ge 0 ]] || (( PICPOS += "${#L_PICS[@]}" ))
+
+    #-----CHANGE-WALLPAPER---------------------------------------------------------#
+    xwallpaper "${PARGS[*]:---focus}" "${L_PICS[ "${PICPOS}" ]}"
+
+    #-----UPDATE-DATA-FILE---------------------------------------------------------#
+    update_data_file
+    #-----NOTIFICATION-------------------------------------------------------------#
+    if [[ "${SILENT}" -eq 0 ]] ; then
+        notify_wallpaper_change
+    fi
+}
+
+main "${@}"
 ####################
 ) #---END-SUBSHELL-#
 ####################
