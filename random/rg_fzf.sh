69b995fc41032f8bc365d6e7f12202d27aad76a0
ediw8311xht <maxballard333@gmail.com>
2024-07-29 21:27:47 -0400

    added fzf rg script

---
+++ random/rg_fzf.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+rg_fzf_func() {
+
+    local lf_script="${HOME}/bin/cd_from_lf.sh"
+    local outarr dir file
+    local RG_PREFIX="rg --no-search-zip --column --line-number --no-heading --color=always --smart-case"
+    local INITIAL_QUERY="${*:-}"
+    local OPTIONS=(
+        --ansi --disabled --query "$INITIAL_QUERY"
+        --bind "start:reload:$RG_PREFIX {q}"
+        --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true"
+        --bind "ctrl-b:become(printf '%s\0' 'browser' {1})"
+        --bind "ctrl-l:become(printf '%s\0' 'lf' {1})"
+        --bind "ctrl-x:execute(xdg-open {1})"
+        --bind "ctrl-a:become(printf '%s\0' 'cd' {1})"
+        --delimiter :
+        --preview 'bat --color=always {1} --highlight-line {2}'
+        --preview-window 'right,50%,border-bottom,+{2}+3/3,~3'
+        --bind "enter:become(${EDITOR} {1} +{2})"
+    )
+    if mapfile -d $'\0' outarr < <(fzf "${OPTIONS[@]}") ; then
+        dir="$(dirname "${outarr[1]}")"
+        file="$(basename "${outarr[1]}")"
+        cd "${dir}" || { echo "Couldnt cd to '${dir}'"; exit 1; }
+        case "${outarr[0]}" in
+                 lf) "${lf_script}" "${file}"
+        ;;  browser) "${BROWSER}" "${file}"
+        ;; esac
+    fi
+}
+
+
+rg_fzf_func "${@}"
