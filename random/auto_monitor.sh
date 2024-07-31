9be74ad7fe207374dd97fe8525e1325aa26fcc9b
ediw8311xht <maxballard333@gmail.com>
2024-07-31 19:26:22 -0400

    f

--- random/auto_monitor.sh
+++ random/auto_monitor.sh
@@ -46,7 +46,6 @@ handle_other() {
 
 update_monitor_export() {
     local ORDER_MONITORS=('DP' 'DisplayPort' 'HDMI' 'VGA' 'DVI' 'TV')
-    local ORDER_MONITORS=('HDMI-2' 'DP' 'HDMI-3') #----------IGNORE---------#
     local primary='DP'
     local MONS gmon lmon i
     MONS="$(get_monitors)"
