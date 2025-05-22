97006f0bc874ba3cb7cab5905dd9f4685e080fcb
ediw8311xht <maxballard333@gmail.com>
2025-05-22 14:01:17 -0400

    Update make_run_cpp.sh

--- random/make_run_cpp.sh
+++ random/make_run_cpp.sh
@@ -1,40 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env /usr/bin/bash
 
-get_full_path() {
-    echo
-}
-
-check_files() {
-    if [[ "${#}" -le 0 ]] ; then
-        return 0
-    elif [[ ! -f "${1}" ]] ; then 
-        printf "File Not Found: '%s'\n" "${1}" >&2
-        exit 1
-    else
-        check_files "${@: 2}"
-    fi
-}
-
-main() {
+main_make_run_cpp() {
     local outfile="/tmp/cpp_tmp.out"
     local exit_code
     local run_file=""
     local info_file="./.my_info"
+    local include_files=()
 
-    if [[ "${1##*.}" = "my_info" ]] ; then
-        info_file="${1}"; shift 1
-    fi
-    if [[ -n "${1}" ]] && check_files "${@}" ; then
-        run_file="${*}"
-    elif ! run_file="$(fd -e .cpp -e .h)" ; then
-        echo "No .cpp/.h files found" >&2
-        return 1;
-    fi
+    # Functions
+    check_files() {
+        if [[ "${#}" -le 0 ]] ; then
+            return 0
+        elif [[ ! -f "${1}" ]] ; then 
+            printf "File Not Found: '%s'\n" "${1}" >&2
+            exit 1
+        else
+            check_files "${@: 2}"
+        fi
+    }
+    handle_args() {
+        while [[ "${#}" -gt 0 ]] ; do
+            if [[ "${1##*.}" =~ [.].*info$ ]] ; then
+                info_file="${1}"
+                shift 1
+            elif [[ "${1,,}" =~ ^-[a-z]$ ]] ; then
+                include_files+=( "${1}" "${2}" )
+                shift 2
+            elif [[ -n "${1}" ]] && check_files "${@}" ; then
+                run_file="${*}"
+                return 0
+            elif ! run_file="$(fd -e .cpp -e .h)" ; then
+                echo "No .cpp/.h files found" >&2
+                return 1
+            else
+                return 0
+            fi
+        done
+    }
 
+    handle_args "${@}"
     if [[ -f "${info_file}" ]] ; then
         xargs g++ -std="c++20" @"${info_file}" -o "${outfile}" <<< "${run_file}"
     else
-        xargs g++ -std="c++20" -o "${outfile}" <<< "${run_file}"
+        xargs g++ -std="c++20" "${include_files[@]}" -o "${outfile}" <<< "${run_file}" 
     fi
 
 
@@ -45,4 +53,4 @@ main() {
     fi
 }
 
-main "${@}"
+main_make_run_cpp "${@}"
