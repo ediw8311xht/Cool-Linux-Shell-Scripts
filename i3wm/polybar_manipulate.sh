4902e7cfe190d1fdd4609850d10f6509e67312ec
ediw8311xht <maxballard333@gmail.com>
2025-06-23 02:54:51 -0400

    Toggle One Polybar with Dmenu

--- i3wm/polybar_manipulate.sh
+++ i3wm/polybar_manipulate.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
 
 # shellcheck disable=SC2120
-polybar_manipulate_main() { #---------- START ------------------#
+main() { #---------- START ------------------#
 
+local BARS
 local CONFIG_DIR="$HOME/.config/polybar/configs/"
 local CONFIG_INI="$HOME/.config/polybar/config.ini"
 local DMENU_SCRIPT="$HOME/bin/my_dmenu.sh"
-local DIS_MODE='all'
+local DEFAULT_BAR="secondarybar"
+
+declare -A BARS
+BARS["i3wm.primary_monitor"]="primarybar"
+BARS["i3wm.other_monitor_1"]="secondarybar"
+BARS["i3wm.other_monitor_2"]="secondarybar"
 
 xrec_get() { xrdb -get "${1}" | grep "."; }
 xrec_set() { xrdb -override <<< "${1}"; }
 xrec_query_parse() { xrdb -query | sed --sandbox -nE "s/${1}/${2}/p"; }
+
 xrec_get_mons() {
     xrec_query_parse '^(i3wm[.][^:]*monitor(_[0-9]+)?)[:]\s*(.*)\s*$' '\1 \3'
 }
+
 xrec_set_none() {
     xrdb -override \
-        < <(xrec_query_parse '^(i3wm[.][^:]*_monitor[^:]*_pid[:]\s)(.*)$' '\1NONE')
+        < <(xrec_query_parse '^(i3wm[.][^:]*_monitor_pid[:]\s)(.*)$' '\1NONE')
 }
-polybar_kill_one() {
+
+pl_kill() {
     local OTHER
-    if OTHER="$(xrdb -get "${1}" | grep -Pox '\s*[0-9]+\s*')" ; then
-        polybar-msg -P "${OTHER}" "quit"
+    if OTHER="$(xrdb -get "${1}_pid" | grep -Pox '\s*[0-9]+\s*')" ; then
+        xrec_set "${1}_pid: NONE"
+        polybar-msg -p "${OTHER}" "cmd" "quit" 
+        return $?
+    else
+        return 1
     fi
 }
-polybar_kill_all() {
-    polybar-msg "cmd" "quit" && xrec_set_none
+
+pl_toggle_dmenu() { 
+    local NAME VALUE
+    if read -r NAME VALUE < <( "${DMENU_SCRIPT}" < <(xrec_get_mons) ) ; then
+        toggle_polybar "${NAME}" "${VALUE}"
+    else
+        return 1
+    fi
 }
 
-launch_polybar() {
-    local name
-    local value
+pl_kill_all() { polybar-msg "cmd" "quit" && xrec_set_none; }
 
-    polybar_kill_all
+pl_launch() {
+    MONITOR="${1}" nohup polybar --reload "${BARS["${2}"]:-"${DEFAULT_BAR}"}" > /dev/null & disown
+    xrec_set "${2}_pid: ${!}"
+}
+
+pl_launch_all() {
+    local name; local value
+
+    pl_kill_all
 
     while read -r -d $'\n' name value ; do
-        if [[ "${name}" =~ "primary" ]] ; then
-            MONITOR="${value}" nohup polybar --reload primarybar > /dev/null & disown
-        else
-            MONITOR="${value}" nohup polybar --reload secondarybar > /dev/null & disown
-        fi
-        xrec_set "${name}_pid: ${!}"
+        pl_launch "${value}" "${name}"
     done < <(xrec_get_mons)
 }
 config_select() {
@@ -48,18 +68,16 @@ config_select() {
         killall "polybar"
         trash-put "${CONFIG_INI}"
         ln -s "${CONFIG_DIR}/${1}" "${CONFIG_INI}"
-        launch_polybar & disown
+        pl_launch_all & disown
     else
-        echo "ERROR: Config not found '${CONFIG_DIR}/${1}'">&2
         exit 1
     fi
 }
 toggle_polybar() {
-    if pgrep -x 'polybar' ; then
-        polybar_kill_all
-    else
-        launch_polybar "${DIS_MODE}" & disown
-    fi
+    [[ "${1,,}" = 'all' ]] && { pl_kill_all || pl_launch_all; return $?; }
+    echo "${1} ${2}"
+    pl_kill "${1}" || pl_launch "${2}" "${1}"
+
 }
 select_dmenu() {
     local choice=""
@@ -72,25 +90,26 @@ error() {
 }
 move_poly() {
     case "${1,,}" in
-             left)  sed -i --follow-symlinks 's/^\([ \t]*\)offset[-]x/;\1offset-x/'                      "${CONFIG_INI}"
-    ;;      right)  sed -i --follow-symlinks 's/^\([ \t]*\);\([ \t]*\)offset[-]x/\1\2offset-x/'          "${CONFIG_INI}"
-    ;;         up)  sed -i --follow-symlinks 's/^[ \t]*bottom[ \t]*[=][ \t]*true/    bottom = false/'    "${CONFIG_INI}"
-    ;;       down)  sed -i --follow-symlinks 's/^[ \t]*bottom[ \t]*[=][ \t]*false/    bottom = true/'    "${CONFIG_INI}"
+             left)  sed -Ei --follow-symlinks 's/^(\s*)(offset[-]x)/;\1\2/'            "${CONFIG_INI}"
+    ;;      right)  sed -Ei --follow-symlinks 's/^(\s*);(\s*)(offset[-]x)/\1\2\3/'     "${CONFIG_INI}"
+    ;;         up)  sed -Ei --follow-symlinks 's/^(\s*bottom\s*[=]\s*)(true)/\1false/' "${CONFIG_INI}"
+    ;;       down)  sed -Ei --follow-symlinks 's/^(\s*bottom\s*[=]\s*)(false)/\1true/' "${CONFIG_INI}"
     esac
 }
 handle_args() {
     while [[ "${#}" -gt 0 ]] ; do
         case "${1,,}" in
                  move)  move_poly "${2}"; shift
-        ;;     launch)  launch_polybar
-        ;;        all)  DIS_MODE='all'
-        ;;    primary)  DIS_MODE='primary'
-        ;;  secondary)  DIS_MODE='secondary'
-        ;;       show)  launch_polybar
-        ;;       hide)  polybar_kill_all
-        ;;     toggle)  toggle_polybar
+        ;;     launch)  pl_launch_all ; return
+        # ;;        all)  DIS_MODE='all'
+        # ;;    primary)  DIS_MODE='primary'
+        # ;;  secondary)  DIS_MODE='secondary'
+        ;;       show)  pl_launch_all ; return
+        ;;       hide)  pl_kill_all   ; return
+        ;;     toggle)  toggle_polybar 'all' ; return
         ;;     config)  config_select "${2}"; shift
-        ;;      dmenu)  select_dmenu ; return
+        ;;      dmenu)  select_dmenu    ; return
+        ;;     tdmenu)  pl_toggle_dmenu ; return
         ;;          *)  error "Invalid Argument" ; return 1
         esac
         shift
@@ -102,4 +121,4 @@ handle_args "${@}"
 
 } #---------------------------- END ----------------------------#
 
-polybar_manipulate_main "${@}"
+main "${@}"
